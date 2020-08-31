629ba3aad1e21375580e687941538aac387e3acc
Lambourl <65184664+Lambourl@users.noreply.github.com>
2020-08-31 12:36:49 +0200

    Install mxxm in the docker

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -4,3 +4,5 @@ if [[ -z "$GITHUB_TOKEN" ]]; then
 	echo "The GITHUB_TOKEN is required."
 	exit 1
 fi
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/nxxm/nxxm/master/install/install_for_macos_linux.sh)
