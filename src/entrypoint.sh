bcada52a31daa65e4631b37fba6b5d7c524df78d
Lambourl <65184664+Lambourl@users.noreply.github.com>
2020-09-02 12:00:38 +0200

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -7,7 +7,25 @@ fi
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/nxxm/nxxm/master/install/install_for_macos_linux.sh)"
 
-var_path_directory=""
+FILES_LINK=`jq -r '.pull_request._links.self.href' "$GITHUB_EVENT_PATH"`/files
+echo "Files = $FILES_LINK"
 
+curl $FILES_LINK > files.json
+FILES_URLS_STRING=`jq -r '.[].raw_url' files.json`
+
+readarray -t URLS <<<"$FILES_URLS_STRING"
+
+echo "File names: $URLS"
+
+mkdir /usr/local/files
+cd /usr/local/files
+for i in "${URLS[@]}"
+do
+   echo "Downloading $i"
+   curl -LOk --remote-name $i 
+done
+
+echo "Files downloaded!"
+var_path_directory="/usr/local/files"
 
 /usr/local/bin/nxxm $var_path_directory  -t gcc-7-cxx17 --verbose 
