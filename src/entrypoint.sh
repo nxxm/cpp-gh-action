6c65910db6fcce006b69e6086f4060fc24a27bda
Lambourl <65184664+Lambourl@users.noreply.github.com>
2020-09-08 10:45:01 +0200

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -7,27 +7,18 @@ fi
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/nxxm/nxxm/master/install/install_for_macos_linux.sh)"
 
-FILES_LINK=`jq -r '.pull_request._links.self.href' "$GITHUB_EVENT_PATH"`/files
-echo "Files = $FILES_LINK"
 
-curl $FILES_LINK > files.json
-FILES_URLS_STRING=`jq -r '.[].raw_url' files.json`
+var_path_directory="/usr/bin/git"
+var_path_directory2="/usr/bin/git"
+var_path_directory3="/usr/bin/git"
 
-readarray -t URLS <<<"$FILES_URLS_STRING"
 
-echo "File names: $URLS"
 
-mkdir /usr/local/files
-cd /usr/local/files
-for i in "${URLS[@]}"
-do
-   echo "Downloading $i"
-   curl -LOk --remote-name $i 
-done
 
-echo "Files downloaded!"
-var_path_directory="/usr/local/files"
+/usr/local/bin/nxxm /home/runner/work/sandbox/sandbox  -t gcc-7-cxx17 --verbose 
+/usr/local/bin/nxxm /home/runner/work/sandbox/sandbox/.git  -t gcc-7-cxx17 --verbose 
+/usr/local/bin/nxxm /usr/bin/git  -t gcc-7-cxx17 --verbose 
 
-/usr/local/bin/nxxm $var_path_directory  -t gcc-7-cxx17 --verbose 
- /usr/local/bin/nxxm /home/runner/work/sandbox/sandbox -t gcc-7-cxx17 --verbose 
+
+
 
