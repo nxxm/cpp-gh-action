01d7de164b1be20e84de7614a263ff8f6c764f0a
Lambourl <65184664+Lambourl@users.noreply.github.com>
2020-09-10 12:01:32 +0200

    :wrench: use nxxm with option

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -8,20 +8,27 @@ fi
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/nxxm/nxxm/master/install/install_for_macos_linux.sh)"
 
 cd $GITHUB_WORKSPACE
-/usr/local/bin/nxxm $GITHUB_WORKSPACE  -t gcc-7-cxx17 --verbose 
+
+
+if [ "$INPUT_ONLY_EXE" == "enable" ]
+ then
+/usr/local/bin/nxxm $GITHUB_WORKSPACE  -t "$INPUT_TARGET" -C "$INPUT_CONFIGURATION" -j "$INPUT_CPUS" -o --verbose 
 if [ $? -ne 0 ]; then
 echo "An error has occurred."
 echo "Please look at the logs."
 	exit 1
 fi
+ else
+ /usr/local/bin/nxxm $GITHUB_WORKSPACE  -t "$INPUT_TARGET" -C "$INPUT_CONFIGURATION" -j "$INPUT_CPUS" -o --verbose 
+ if [ $? -ne 0 ]; then
+echo "An error has occurred."
+echo "Please look at the logs."
+	exit 1
+fi
+fi
 
 zip -r build.zip ./build
 
-echo '************************ test luc ************************'
-echo "$INPUT_TARGET" 
-echo "$INPUT_CONFIGURATION"
-echo "$INPUT_ONLY_EXE"
-echo "$INPUT_CPUS"
 
 
 
