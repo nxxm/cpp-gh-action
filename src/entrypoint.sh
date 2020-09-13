0ab386c07a52f224f2c14436a1dbb070b9dda792
Lambourl <65184664+Lambourl@users.noreply.github.com>
2020-09-13 19:36:06 +0200

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -4,10 +4,6 @@ if [[ -z "$GITHUB_TOKEN" ]]; then
 	echo "The GITHUB_TOKEN is required."
 	exit 1
 fi
-cd /usr
-cd ..
-ls -laR
-
 var_dir=""
 
 if [[ -z "$INPUT_DIR" ]]; then
@@ -18,7 +14,7 @@ fi
 
 cd $GITHUB_WORKSPACE
 
- /usr/local/bin/nxxm $var_dir $INPUT_TARGET  $INPUT_TEST $INPUT_CONFIG $INPUT_EXCLUDE $INPUT_JOBS $INPUT_EXCLUDE_TEST $INPUT_EXTRA_ARGS $INPUT_USE_CMAKELISTS
+nxxm $var_dir $INPUT_TARGET  $INPUT_TEST $INPUT_CONFIG $INPUT_EXCLUDE $INPUT_JOBS $INPUT_EXCLUDE_TEST $INPUT_EXTRA_ARGS $INPUT_USE_CMAKELISTS
  if [ $? -ne 0 ]; then
 echo "An error has occurred."
 echo "Please look at the logs."
