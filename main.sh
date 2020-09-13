d9a5b58ef2e822d8c833d95e3920c0389d3be27f
Lambourl <65184664+Lambourl@users.noreply.github.com>
2020-09-13 15:18:03 +0200

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+	echo "The GITHUB_TOKEN is required."
+	exit 1
+fi
+
+
+var_dir=""
+
+if [[ -z "$INPUT_DIR" ]]; then
+var_dir="$GITHUB_WORKSPACE"
+else
+var_dir="$GITHUB_WORKSPACE""/""$INPUT_DIR"
+fi
+
+cd $GITHUB_WORKSPACE
+
+ nxxm $var_dir $INPUT_TARGET  $INPUT_TEST $INPUT_CONFIG $INPUT_EXCLUDE $INPUT_JOBS $INPUT_EXCLUDE_TEST $INPUT_EXTRA_ARGS $INPUT_USE_CMAKELISTS
+ if [ $? -ne 0 ]; then
+echo "An error has occurred."
+echo "Please look at the logs."
+	exit 1
+fi
+
+
+
+
+
+
