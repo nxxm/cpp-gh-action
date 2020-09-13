ac8ec7a982d2d959010ff745d00c8afdcf6db415
Lambourl <65184664+Lambourl@users.noreply.github.com>
2020-09-13 14:21:48 +0200

    :wrench: add dir and remove sources

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -7,9 +7,17 @@ fi
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/nxxm/nxxm/master/install/install_for_macos_linux.sh)"
 
+var_dir=""
+
+if [[ -z "$INPUT_DIR" ]]; then
+var_dir="$GITHUB_WORKSPACE"
+else
+var_dir="$GITHUB_WORKSPACE""/""$INPUT_DIR"
+fi
+
 cd $GITHUB_WORKSPACE
 
- /usr/local/bin/nxxm $GITHUB_WORKSPACE $INPUT_TARGET $INPUT_SOURCES $INPUT_TEST $INPUT_CONFIG $INPUT_EXCLUDE $INPUT_JOBS $INPUT_EXCLUDE_TEST $INPUT_EXTRA_ARGS $INPUT_USE_CMAKELISTS
+ /usr/local/bin/nxxm $var_dir $INPUT_TARGET  $INPUT_TEST $INPUT_CONFIG $INPUT_EXCLUDE $INPUT_JOBS $INPUT_EXCLUDE_TEST $INPUT_EXTRA_ARGS $INPUT_USE_CMAKELISTS
  if [ $? -ne 0 ]; then
 echo "An error has occurred."
 echo "Please look at the logs."
