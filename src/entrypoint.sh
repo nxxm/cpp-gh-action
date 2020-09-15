4681f41d6d17de7aba4f252ecc36502ba4cf5d08
Lambourl <65184664+Lambourl@users.noreply.github.com>
2020-09-15 15:31:08 +0200

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -20,6 +20,8 @@ ls -la
 var_dir="$GITHUB_WORKSPACE""$INPUT_DIR"
 
 nxxm $var_dir $INPUT_TARGET  $INPUT_TEST $INPUT_CONFIG $INPUT_EXCLUDE $INPUT_JOBS $INPUT_EXCLUDE_TEST $INPUT_EXTRA_ARGS $INPUT_USE_CMAKELISTS
+
+echo "***"
 if [ $? -ne 0 ]; then
 echo "An error has occurred."
 echo "Please look at the logs."
